a6ddba6950e450acb3f5a5c121ee7ff5cf7299d5
Vamsi Krishna Srungarapu <vamsi.krishna@commonfloor.com>
2015-01-03 14:34:17 +0530

    given a url, current code extracts the urls in th ehtml corresponding to the given url

---
+++ extract_url.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+)
+
+func extractURLS(url string) []string {
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	urls := make([]string, 0)
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		url, _ := s.Attr("href")
+		urls = append(urls, url)
+	})
+	return urls
+}
+
+func main() {
+	urls := extractURLS("http://www.indiabix.com/")
+	for _, url := range urls {
+		fmt.Printf("New URL : %s\n", url)
+	}
+}
